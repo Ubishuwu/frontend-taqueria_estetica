a1c2b5be5d094770ea3df8b40379298edc954989
Abyss215 <cristian21rv15@outlook.com>
2024-03-31 22:06:20 -0600

    inicio :v

---
+++ src/components/views/Caja.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eligendi saepe deleniti sit amet quidem molestias pariatur eos deserunt incidunt quaerat quis, tenetur dicta minima aperiam eum totam modi quasi?</p>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+p{
+    background-color: aquamarine;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { router } from "./routes/index.js";
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-createApp(App).mount('#app')
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')

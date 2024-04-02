6af50d515c38d00d84d9859c0b00839afae194a6
Abyss215 <cristian21rv15@outlook.com>
2024-04-01 19:45:31 -0600

    tarjetas para productos o servicio

---
+++ src/components/elementos/Tarjeta.vue
@@ -0,0 +1,278 @@
+<template>
+    <div class="content" ref="princi">
+        <div class="ficha" ref="ficha">
+            <div class="cantidad" ref="aumentador">
+                <button @click="disminuir()" id="izq">-</button>
+                <p>{{ canti }}</p>
+                <button @click="aumentar()" id="der">+</button>
+            </div>
+
+
+            <img class="imag" ref="imag" src="../../assets/miku.png" alt="user">
+            <div class="data" ref="data" @click="activar()">
+                <h2>{{ titulo }}</h2>
+                <p class="tipo">{{ tipo }}</p>
+                <p class="precio">${{ precio }}</p>
+            </div>
+        </div>
+        <button class="agregar" ref="boton" @click="agregar()">agregar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titulo: {
+            default: 'titulo',
+        },
+        tipo: {
+            default: "orden",
+        },
+        precio: {
+            default: "49.80",
+        },
+    },
+    data() {
+        return {
+            canti: 0
+        }
+    },
+    computed: {},
+    methods: {
+        activar() {
+            if (!this.$refs.princi.classList.contains("activo")) {
+                this.$refs.princi.classList.add('activo');
+                this.$refs.ficha.classList.add('activo');
+                this.$refs.aumentador.classList.add('activo');
+                this.$refs.imag.classList.add('activo');
+                this.$refs.data.classList.add('activo');
+                this.$refs.boton.classList.add('activo');
+            }
+        },
+        aumentar() {
+            this.canti++;
+        },
+        disminuir() {
+            if (this.canti != 0)
+                this.canti--;
+        },
+        agregar() {
+            this.base();
+            this.canti = 0;
+        },
+        base() {
+            this.$refs.princi.classList.remove('activo');
+            this.$refs.ficha.classList.remove('activo');
+            this.$refs.aumentador.classList.remove('activo');
+            this.$refs.imag.classList.remove('activo');
+            this.$refs.data.classList.remove('activo');
+            this.$refs.boton.classList.remove('activo');
+        }
+    }
+}
+</script>
+
+<style scoped>
+/**normal */
+.content {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+
+    position: relative;
+    width: 80%;
+
+    margin: 1rem;
+    padding-top: 0rem;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+
+    padding-bottom: auto;
+}
+
+h2 {
+    font-size: larger;
+    font-variant: small-caps;
+}
+
+.tipo {
+    border-radius: 20px;
+    font-size: small;
+    background-color: rgb(178, 176, 176);
+    margin: 0px 8%;
+}
+
+.precio {
+    color: rgb(8, 119, 8);
+    -webkit-text-stroke: 1px rgb(12, 141, 46);
+}
+
+/**estructura ficha */
+.ficha {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    border-radius: 20px;
+    background-color: rgba(8, 8, 74, 0);
+
+
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+
+    padding: auto;
+
+    transition-property: background-color;
+    transition-duration: 0.5s;
+    transition-timing-function: ease-in;
+}
+
+
+.data {
+    border-radius: 20px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(185, 186, 187);
+    background-color: rgb(247, 247, 247);
+
+    /*config solo*/
+    padding-top: 30%;
+    margin-top: 30%;
+
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+
+    text-align: center;
+
+    transition-property: margin;
+    transition-duration: 0.5s;
+    transition-timing-function: ease-in-out;
+
+}
+
+.imag {
+    border-radius: 100%;
+    position: absolute;
+    object-fit: cover;
+    left: 20%;
+    top: 0;
+    width: 60%;
+    aspect-ratio: 1/1;
+}
+
+/***pagar-cantidad */
+.imag.activo {
+    display: none;
+}
+
+.content.activo {
+    /**para pagar */
+    padding-bottom: 15px;
+}
+
+.ficha.activo {
+
+    /**compra */
+    padding: 15px;
+    background-color: rgb(8, 8, 74);
+    transition: background-color 0.5s ease;
+}
+
+.data.activo {
+    /**config compra */
+    margin: 0;
+    margin-bottom: 10px;
+    padding: 0px;
+    background-color: rgba(240, 248, 255, 0);
+    border-width: 0;
+    color: white;
+    height: 100%;
+    transition-property: margin;
+    transition-duration: 0.5s;
+    transition-timing-function: ease-in-out;
+
+}
+
+.cantidad,
+.agregar {
+    display: none;
+}
+
+
+button {
+    padding: 5px;
+    background-color: white;
+}
+
+#izq {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+}
+
+#der {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+}
+
+.cantidad.activo {
+    border-radius: 15px;
+    display: flex;
+    flex-direction: row;
+    width: 80%;
+    aspect-ratio: 3/1;
+    background-color: white;
+    justify-content: space-between;
+    align-items: center;
+
+    font-size: large;
+    margin: 5px;
+}
+
+.cantidad button {
+    height: 100%;
+    aspect-ratio: 1/1;
+}
+
+.agregar.activo {
+    display: block;
+    width: 80%;
+    border-radius: 10px;
+    position: absolute;
+    background-color: green;
+    bottom: 0;
+    color: white;
+
+}
+
+button:hover {
+    background-color: rgb(230, 229, 229);
+}
+
+.agregar:hover {
+    background-color: rgb(9, 170, 9);
+}
+
+
+@media all and (min-width: 420px) {
+    .content {
+        width: 42%;
+    }
+}
+
+@media all and (min-width: 600px) {
+    .content {
+        width: 25%;
+    }
+}
+
+@media all and (min-width: 900px) {
+    .content {
+        width: 20%;
+    }
+
+}
+</style>
--- src/components/views/Caja.vue
+++ src/components/views/Caja.vue
@@ -1,12 +1,32 @@
 <template>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eligendi saepe deleniti sit amet quidem molestias pariatur eos deserunt incidunt quaerat quis, tenetur dicta minima aperiam eum totam modi quasi?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eligendi saepe deleniti sit amet quidem
+        molestias pariatur eos deserunt incidunt quaerat quis, tenetur dicta minima aperiam eum totam modi quasi?</p>
+
+    <div>
+        <Tarjeta titulo="agua de jamaica con trozo de piña owo" tipo="1 vaso" precio="25.00" v-for="numero in 10" :key="numero" />
+        <Tarjeta titulo="papas con chilito" tipo="1 vaso" precio="25.00" />
+        <Tarjeta titulo="marzopa" tipo="1 vaso" precio="25.00" />
+        <Tarjeta titulo="mazapan" tipo="1 vaso" precio="25.00" />
+    </div>
 </template>
 
-<script>
+<script setup>
+import Tarjeta from '../elementos/Tarjeta.vue'
+
 </script>
 
 <style scoped>
-p{
+p {
     background-color: aquamarine;
+}
+
+div {
+
+    background-color: rgb(230, 229, 229);
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
 }
 </style>

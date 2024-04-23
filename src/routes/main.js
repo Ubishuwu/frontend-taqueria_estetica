5df20b92ef9419623d6716bdfa129a6d38726cef
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-04-23 14:28:07 -0600

    feat(cocina.vue createplatillo.vue): creación de las vistas para Cocina

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,7 +3,8 @@ import Home from '../components/views/Home.vue'
 import Caja from '../components/views/Caja.vue'
 import Inventario from '../components/views/Inventario.vue'
 import Login from '../components/forms/Login.vue';
-
+import Cocina from '../components/views/Cocina.vue';
+import CreatePlatillo from '../components/views/CreatePlatillo.vue';
 const routes = [
   {
     path: "/",
@@ -19,12 +20,22 @@ const routes = [
         name: "Inventario",
         component: Inventario,
       },
+      {
+        path: "cocina",
+        name: "Cocina",
+        component: Cocina,
+      },
     ]
   },
   {
     path: "/Login",
   name: "Login",
   component: Login
+},
+{
+  path: "/create-platillo",
+name: "Crear Plantillo",
+component: CreatePlatillo
 }
 ];
 

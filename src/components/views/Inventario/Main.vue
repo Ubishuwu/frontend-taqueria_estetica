fbfed245eea261bdd08306882f3f347368eaaa52
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-04-23 19:03:07 -0600

    feat(inventario/): creación de las página para el sidebar del inventario

--- src/components/views/Inventario/Main.vue
+++ src/components/views/Inventario/Main.vue
@@ -1,39 +1,37 @@
 <template>
-  <div>
-    <form class="flex">
-      <label class="form-control w-full max-w-xs mr-2">
-        <div class="label">
-          <span class="label-text">Sucursal</span>
+  <div class="stats shadow">
+        <div class="stat bg-slate-300">
+          <div class="stat-figure text-primary">
+            
+          </div>
+          <div class="stat-title">Ventas</div>
+          
+          <div class="stat-value text-primary">$20,000</div>
+          <div class="stat-desc text-error">
+              <font-awesome-icon :icon="['fas', 'arrow-trend-down']" />
+              90 (14%)</div>
+              <div class="stat-desc text-error">
+                  $1,000 para recuperar la inversion
+              </div>
         </div>
-        <select class="select select-bordered">
-          <option>Todos</option>
-          <option>Taqueria El TaquerITO</option>
-          <option>Estetica El cortITO</option>
-        </select>
-      </label>
-      <label class="form-control w-full max-w-xs mr-2">
-        <div class="label">
-          <span class="label-text">Estatus</span>
+  
+        <div class="stat bg-[#d5e4fa]">
+          <div class="stat-figure text-3xl">
+              <font-awesome-icon :icon="['fas', 'utensils']" />
+          </div>
+          <div class="stat-title">Taqueria</div>
+          <div class="stat-value ">- $10,000</div>
+          <div class="stat-desc">21% mas que el mes pasado</div>
         </div>
-        <select class="select select-bordered">
-          <option>Todos</option>
-          <option>Proximos a vencer</option>
-          <option>Normal</option>
-          <option>Sobreinventario</option>
-        </select>
-      </label>
-      <label class="form-control w-full max-w-xs mr-2">
-        <div class="label">
-          <span class="label-text">Ordenar por</span>
+        <div class="stat bg-[#e5d6fb] ">
+          <div class="stat-figure text-3xl">
+              <font-awesome-icon :icon="['fas', 'store']" />
+          </div>
+          <div class="stat-title">Taqueriaz</div>
+          <div class="stat-value ">- $10,000</div>
+          <div class="stat-desc">21% mas que el mes pasado</div>
         </div>
-        <select class="select select-bordered">
-          <option>Nombre</option>
-          <option>Inventario</option>
-          <option>Sucursal</option>
-        </select>
-      </label>
-    </form>
-  </div>
+      </div>
 </template>
 <script></script>
 
